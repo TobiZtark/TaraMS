71d000a5107112912197d379b58eab3c8b945a27
Oluwatobiloba Adegbenro <adegbenrooluwatobiloba@gmail.com>
2018-07-06 11:46:14 +0100

    Add files via upload
    The Assets are too large about 7gb. So i added just the scripts and settings.

---
+++ Login.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Login : MonoBehaviour {
+		private string u= "icamlight";
+		private string p= "icamlight";
+		
+		private string usernameString = string.Empty;
+		private string passwordString = string.Empty;
+		
+		private Rect windowrect =new Rect (0,0,Screen.width, Screen.height); 
+		
+		void Start ()
+		{
+			
+		}
+		
+		void Update() {
+			
+		}
+		
+		void OnGUI()
+		{
+			GUI.Window(0, windowrect, WindowFunction,"Login");
+		}
+		
+		void WindowFunction(int windowID)
+		{
+			usernameString = GUI.TextField(new Rect(Screen.width/3,2* Screen.height/5,Screen.width/3, Screen.height/10),usernameString,10);
+			passwordString = GUI.PasswordField(new Rect(Screen.width/3,2* Screen.height/3,Screen.width/3, Screen.height/10),passwordString,"*"[0],10);
+			if(GUI.Button(new Rect(Screen.width/2, 4* Screen.height/5, Screen.width/8, Screen.height/8), "Login"))
+			{
+				if (usernameString==u && passwordString==p)
+				{
+					Debug.Log("Successful");
+					Application.LoadLevel(2);
+				}
+				else { Debug.Log ("Try Again"); Application.LoadLevel(1);}
+			}
+			GUI.Label(new Rect(Screen.width/3,35*Screen.height/100,Screen.width/5,Screen.height/8),"Username");
+			GUI.Label(new Rect(Screen.width/3,62*Screen.height/100,Screen.width/5,Screen.height/8),"Password");
+	}
+}
---
+++ Playerpicki.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Playerpicki : MonoBehaviour {
+	
+	public AudioSource sounds;
+	private int count;
+	public Text Count;
+	private GameObject DisplayText;
+	private GameObject WinCanvas;
+	private GameObject Tara;
+	private GameObject Auny;
+	Animator anim;
+	int pickHash= Animator.StringToHash("Pick");
+	// Use this for initialization
+	void Awake(){
+		DisplayText = GameObject.Find ("DisplayText");
+		WinCanvas = GameObject.Find ("WinCanvas");
+		Tara = GameObject.Find ("Tara");
+		Auny = GameObject.Find ("Auny Zira");
+	}
+	void Start () {
+		anim = GetComponent<Animator> ();
+		count = 0;
+		SetCount ();
+		sounds = GetComponent<AudioSource> ();
+		DisplayText.SetActive (true);
+		WinCanvas.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (count==5) {
+			WinCanvas.SetActive (true);
+			DisplayText.SetActive (false);
+			Tara.SetActive(false);
+			Auny.SetActive(false);
+			//Application.LoadLevel("Game 0");
+		}
+	}
+	void OnTriggerEnter (Collider other)
+	{
+		
+		if (other.gameObject.tag == "Pickable")
+		{
+			//anim.SetTrigger(pickHash);
+			other.gameObject.SetActive(false);
+			count = count +1; 
+			SetCount ();
+			sounds.Play ();
+		}
+		
+	}
+	
+	void SetCount()
+	{
+		Count.text = "Score: " + count.ToString ();
+	}
+}
